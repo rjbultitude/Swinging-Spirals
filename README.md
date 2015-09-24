320c837d9769eafef95f0a38fa8d44129f6f7871
rjbultitude <richard.bultitude@gmail.com>
2015-09-24 21:50:25 +0100

    add files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Swinging Spirals
+
+##Live demo
+[Live demo on github pages](http://rjbultitude.github.io/Swinging-Spirals/)
+
+## Info
+This canvas sketch generates faux-3D animated sprials whose swing amount can be adjusted.
+Spirals can be added or removed using the additional controls.
+
+## References
+
+* [P5.js](http://p5js.org/)
---
+++ sketch.js
@@ -0,0 +1,105 @@
+var circleArr = [];
+var myCanvas;
+var num = 130;
+var twidth = 5;
+var swing = 20;
+var noiseStart = 0.1;
+var numSpirals = 1;
+
+function Longline(xpos, ypos, radius, colour, amt) {
+	this.xpos = xpos;
+	this.ypos = ypos;
+	this.radius = radius;
+	this.amt = amt;
+	this.colour = color(random(colour, 255), colour - 50, colour - 800, 255);
+}
+
+Longline.prototype.paint = function() {
+	fill(this.colour);
+	noStroke();
+	ellipse(this.xpos, this.ypos, this.radius, this.radius);
+}
+
+Longline.prototype.update = function() {
+	var prevXpos = this.xpos;
+	var prevYpos = this.ypos;
+	var prevRadius = this.radius;
+
+	var angle = frameCount/50;
+
+	this.xpos = prevXpos + sin(angle) * this.amt/swing;
+	this.ypos = prevYpos + sin(angle) * this.amt/(swing/2);
+}
+
+function createLineArr(newOffset, newColourSeed, newRadAngle, newInnerRadius) {
+
+	//Spiral logic
+	var innerRadius = newInnerRadius || 20;
+	var offset = newOffset || 10;
+	var radius = 5;
+	var rad = 0;
+	var radAngle = newRadAngle || 10;
+	//colour
+	var colour = newColourSeed || 30;
+		
+	for (var i = 0; i < num; i++) {
+
+		innerRadius += 2;
+		radius += 0.2;
+		rad = radians(i * radAngle);
+		noiseStart += 0.1;
+		colour = 30 + noise(noiseStart) * 200;
+
+		var xpos = sin(rad) * innerRadius + (width/2);
+		var ypos = cos(rad) * innerRadius + (height/2) - offset * i/4;
+		var newLongline = new Longline(xpos, ypos, radius, colour, i);
+		circleArr.push(newLongline);
+	}
+}
+
+//controls
+var addBtn = document.getElementById('add');
+var subBtn = document.getElementById('subtract');
+var addSpiral = document.getElementById('add-spiral');
+var subSpiral = document.getElementById('sub-spiral');
+
+//add spiral
+addSpiral.addEventListener('click', function() {
+	createLineArr(random(20,30), random(50,100), random(12,24), random(10, 100));
+	numSpirals += 1;
+});
+
+//remove spiral
+subSpiral.addEventListener('click', function() {
+	var segment = circleArr.length / num;
+	circleArr.splice(segment - 1, num);
+});
+
+//increase swing
+addBtn.addEventListener('click', function() {
+	if (swing > 5) {
+		swing -= 5;
+	}
+});
+
+//decrease swing
+subBtn.addEventListener('click', function() {
+	if (swing < 300) {
+		swing += 5;
+	}
+});
+
+function setup() {
+	myCanvas = createCanvas(800,600);
+	myCanvas.parent('canvas-container');
+	background(0,0,0);
+	createLineArr();
+}
+
+function draw() {
+	background(0, 0, 0);
+	for (var i = 0; i < circleArr.length; i++) {
+		circleArr[i].paint();
+		circleArr[i].update();
+	}
+}
